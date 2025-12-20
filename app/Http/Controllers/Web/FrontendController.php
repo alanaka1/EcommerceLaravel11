8c73eca982fc1abb6d13cfe956559d7c770aca48
alanaka1 <alanaka1@hotmail.com>
2025-12-20 23:34:42 +0300

    2 - تركيب تصميم الفرونت اند في المشروع ج1 - دورة بناء متجر الكتروني باستخدام لارافيل Laravel 11 https://www.youtube.com/watch?v=5Iha50vIYjY&list=PLeRGxvo-jizOuyWj9NN7Xp1WR6OrdpKtq&index=2

---
+++ app/Http/Controllers/Web/FrontendController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class FrontendController extends Controller
+{
+    public function home()
+    {
+        return View('Ecommerce.Web.Template.layouts.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\FrontendController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::controller(FrontendController::class)->group(function () {
+    Route::get('/', 'home');
 });
+
