4630751582aecf699da2622bca5b546e6907cfa6
alanaka1 <alanaka1@hotmail.com>
2025-12-21 22:52:27 +0300

    Auth Login and Register 2

--- resources/views/Ecommerce/Web/Auth/login.blade.php
+++ resources/views/Ecommerce/Web/Auth/login.blade.php
@@ -9,7 +9,6 @@
 @section('content')
 
 <div class="card-body p-4">
-
     <div class="text-center mb-4">
         <i class="fas fa-user-shield fa-3x text-primary mb-2"></i>
         <!-- <h4>تسجيل الدخول</h4> -->
@@ -18,41 +17,41 @@
         <p class="text-muted small">Enter your details to continue</p>
     </div>
 
-    <form>
-        <div class="mb-3">
-            <!-- <label class="form-label">البريد الإلكتروني</label> -->
-            <label class="form-label">Email</label>
-            <input type="email" class="form-control" placeholder="example@email.com">
-        </div>
-
-        <div class="mb-3">
-            <!-- <label class="form-label">كلمة المرور</label> -->
-            <label class="form-label">Password</label>
-            <input type="password" class="form-control" placeholder="********">
-        </div>
-
-        <div class="d-flex justify-content-between mb-3">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox">
-                <!-- <label class="form-check-label">تذكرني</label> -->
-                <label class="form-check-label">Remember me</label>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+            <div class="mb-3">
+                <!-- <label class="form-label">البريد الإلكتروني</label> -->
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="example@email.com">
             </div>
-            <!-- <a href="#" class="small">نسيت كلمة المرور؟</a> -->
-            <a href="#" class="small">Forgot your password?</a>
-        </div>
 
-        <button class="btn btn-primary w-100">
-            Login
-        </button>
-    </form>
+            <div class="mb-3">
+                <!-- <label class="form-label">كلمة المرور</label> -->
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" placeholder="********">
+            </div>
 
-    <p class="text-center mt-3 small">
-        <!-- ما عندك حساب؟ -->
-        Don't you have an account?
-        <!-- <a href="register.html">إنشاء حساب</a> -->
-        <a href="{{ route('register') }}">Create an account</a>
-    </p>
+            <div class="d-flex justify-content-between mb-3">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox">
+                    <!-- <label class="form-check-label">تذكرني</label> -->
+                    <label class="form-check-label">Remember me</label>
+                </div>
+                <!-- <a href="#" class="small">نسيت كلمة المرور؟</a> -->
+                <a href="#" class="small">Forgot your password?</a>
+            </div>
 
+            <button class="btn btn-primary w-100">
+                Login
+            </button>
+        </form>
+
+        <p class="text-center mt-3 small">
+            <!-- ما عندك حساب؟ -->
+            Don't you have an account?
+            <!-- <a href="register.html">إنشاء حساب</a> -->
+            <a href="{{ route('register') }}">Create an account</a>
+        </p>
 </div>
 
 @endsection
--- resources/views/Ecommerce/Web/Auth/register.blade.php
+++ resources/views/Ecommerce/Web/Auth/register.blade.php
@@ -18,29 +18,30 @@
         <p class="text-muted small">Create your account in one minute</p>
     </div>
 
-    <form>
+    <form method="POST" action="{{ route('register') }}">
+            @csrf
         <div class="mb-3">
             <!-- <label class="form-label">الاسم الكامل</label> -->
             <label class="form-label">Full Name</label>
-            <input type="text" class="form-control" placeholder='Full Name'>
+            <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder='Full Name'>
         </div>
 
         <div class="mb-3">
             <!-- <label class="form-label">البريد الإلكتروني</label> -->
             <label class="form-label">Email</label>
-            <input type="email" class="form-control" placeholder='Email'>
+            <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder='Email'>
         </div>
 
         <div class="mb-3">
             <!-- <label class="form-label">كلمة المرور</label> -->
             <label class="form-label">Password</label>
-            <input type="password" class="form-control" placeholder="********">
+            <input type="password" class="form-control" name="password" placeholder="********">
         </div>
 
         <div class="mb-3">
             <!-- <label class="form-label">تأكيد كلمة المرور</label> -->
             <label class="form-label">Confirm Password</label>
-            <input type="password" class="form-control" placeholder="********">
+            <input type="password" class="form-control" name="password_confirmation" placeholder="********">
         </div>
 
         <button class="btn btn-success w-100">
