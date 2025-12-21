42f43cd37cc064df64a43accba01bea829d91863
alanaka1 <alanaka1@hotmail.com>
2025-12-21 17:14:06 +0300

    6 - مكتبة Spatie لعمل نظام الصلاحيات و الادوار - دورة بناء متجر الكتروني باستخدام لارافيل Laravel 11 //// https://www.youtube.com/watch?v=ND6MX9axYrs&list=PLeRGxvo-jizOuyWj9NN7Xp1WR6OrdpKtq&index=6

--- app/Http/Controllers/Web/FrontendController.php
+++ app/Http/Controllers/Web/FrontendController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class FrontendController extends Controller
 {
     public function home()
     {
+        // https://spatie.be/docs/laravel-permission/v6/installation-laravel
+        // $role = Role::create(['name' => 'user']);
+        // $permission = Permission::create(['name' => 'user']);
         return View('Ecommerce.Web.Template.index');
     }
 }
