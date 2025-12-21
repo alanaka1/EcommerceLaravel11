5d17d8a9b57f4b0ecbba1a5ce46942dad9a79401
alanaka1 <alanaka1@hotmail.com>
2025-12-21 15:26:00 +0300

    4 - نظام تسجيل الدخول و انشاء حساب جديد Breeze - دورة بناء متجر الكتروني باستخدام لارافيل Laravel 11 https://www.youtube.com/watch?v=yUejJj1VGaI&list=PLeRGxvo-jizOuyWj9NN7Xp1WR6OrdpKtq&index=4

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\FrontendController;
-
-
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -13,3 +12,14 @@ Route::controller(FrontendController::class)->group(function () {
     Route::get('/', 'home');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
